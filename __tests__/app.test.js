ca742c24538765643406321235b7bd40e1aee9ce
Kubi Kubisiak <mckubisiak@gmail.com>
2021-08-02 09:43:59 -0700

    intial test passing for post

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,19 +2,26 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Goal from '../lib/models/goal.js';
 
-describe('demo routes', () => {
+describe('goal routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('creates a new goal', () => {
-    return request(app)
-      .post('api/v1/goals')
-      .send({ goal: 'UPDATED' })
-      .then((res) => {
-        expect(res.body).toEqual({ goal: 'UPDATED' });
-      });
+  it('creates a new goal', async () => {
+    const goal1 = {
+      description: 'UPDATED',
+      achieved: true,
+      discovery: '2011-01-01T08:00:00.000Z' 
+    };
+
+    const res = await request(app).post('/api/v1/goals').send(goal1);
+    expect(res.body).toEqual({
+      id: '1',
+      ...goal1,
+    });
+
   });
 
 });
